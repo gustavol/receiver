cce0d8a0805618480a89e92dd51e6a2e0c5dffa4
Luiz Gustavo <gustavo@plotrf.com.br>
2024-04-17 18:52:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,60 +13,68 @@ function stop () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.clearScreen()
 }
+/**
+ * P0= Esq Tras
+ * 
+ * P1 = Esq Frente
+ * 
+ * P2 = Dir frente
+ * 
+ * P16 = Dir tras
+ */
 function handleReceivedMessage (receivedNumber: number) {
     if (receivedNumber == 0) {
         stop()
     } else if (receivedNumber == 1) {
         if (direction == 0) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            pins.digitalWritePin(DigitalPin.P1, 1)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.analogWritePin(AnalogPin.P0, 0)
+            pins.analogWritePin(AnalogPin.P1, maxDuty - 100)
+            pins.analogWritePin(AnalogPin.P2, maxDuty - 100)
+            pins.analogWritePin(AnalogPin.P16, 0)
         } else {
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.analogWritePin(AnalogPin.P0, maxDuty - 100)
+            pins.analogWritePin(AnalogPin.P1, 0)
+            pins.analogWritePin(AnalogPin.P2, 0)
+            pins.analogWritePin(AnalogPin.P16, maxDuty - 100)
         }
     } else if (receivedNumber == 2) {
         if (direction == 0) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            pins.digitalWritePin(DigitalPin.P1, 1)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            pins.digitalWritePin(DigitalPin.P16, 0)
+            pins.analogWritePin(AnalogPin.P0, maxDuty)
+            pins.analogWritePin(AnalogPin.P1, 0)
+            pins.analogWritePin(AnalogPin.P2, maxDuty)
+            pins.analogWritePin(AnalogPin.P16, 0)
         } else {
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.analogWritePin(AnalogPin.P0, 0)
+            pins.analogWritePin(AnalogPin.P1, maxDuty)
+            pins.analogWritePin(AnalogPin.P2, 0)
+            pins.analogWritePin(AnalogPin.P16, maxDuty)
         }
     } else if (receivedNumber == 3) {
-        if (direction == 0) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            pins.digitalWritePin(DigitalPin.P16, 0)
+        if (direction == 1) {
+            pins.analogWritePin(AnalogPin.P0, maxDuty)
+            pins.analogWritePin(AnalogPin.P1, 0)
+            pins.analogWritePin(AnalogPin.P2, maxDuty)
+            pins.analogWritePin(AnalogPin.P16, 0)
         } else {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            pins.digitalWritePin(DigitalPin.P16, 1)
+            pins.analogWritePin(AnalogPin.P0, 0)
+            pins.analogWritePin(AnalogPin.P1, maxDuty)
+            pins.analogWritePin(AnalogPin.P2, 0)
+            pins.analogWritePin(AnalogPin.P16, maxDuty)
         }
     } else if (receivedNumber == 4) {
         direction = 0
     } else if (receivedNumber == 5) {
         direction = 1
-    } else {
-        basic.showIcon(IconNames.No)
     }
 }
 let lastRx = 0
 let direction = 0
 let rxBuffer: number[] = []
+let maxDuty = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
+maxDuty = 900
 basic.forever(function () {
     // stop();
     if (rxBuffer.length > 0) {
